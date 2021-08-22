b58c0e436a6fb7ec44b19b6b260b6d9d7343e21d
Przemysław Cichy <przemyslav.cichy@gmail.com>
2021-08-22 12:44:06 +0200

    add_cnn_solution

---
+++ SnakeController/SnakeController.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <list>
+#include <memory>
+
+#include "IEventHandler.hpp"
+#include "SnakeInterface.hpp"
+#include <stdexcept>
+
+class Event;
+class IPort;
+
+namespace Snake
+{
+struct ConfigurationError : std::logic_error
+{
+    ConfigurationError();
+};
+
+struct UnexpectedEventException : std::runtime_error
+{
+    UnexpectedEventException();
+};
+
+class Controller : public IEventHandler
+{
+public:
+    Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePort, std::string const& p_config);
+
+    Controller(Controller const& p_rhs) = delete;
+    Controller& operator=(Controller const& p_rhs) = delete;
+
+    void receive(std::unique_ptr<Event> e) override;
+
+private:
+    struct Segment
+    {
+        int x;
+        int y;
+        int ttl;
+    };
+
+    IPort& m_displayPort;
+    IPort& m_foodPort;
+    IPort& m_scorePort;
+
+    std::pair<int, int> m_mapDimension;
+    std::pair<int, int> m_foodPosition;
+
+    Direction m_currentDirection;
+    std::list<Segment> m_segments;
+
+    void prepareNewFood(const std::unique_ptr<Event> &e);
+    std::unique_ptr<Event> &getTimePassedPtr(const std::unique_ptr<Event> &e);
+    std::unique_ptr<Event> &getDirectionChangePtr(const std::unique_ptr<Event> &e);
+    std::unique_ptr<Event> &getFoodChangePtr(const std::unique_ptr<Event> &e);
+    Segment getNewHeadSegment();
+};
+
+} // namespace Snake
